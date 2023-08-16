34deeb962ee361677d02e2f43821493b6cf61cda
Vishnu <22044751@myrp.edu.sg>
2023-08-16 23:33:58 +0800

    Administrator Class

--- C206_CaseStudy/src/Administrator.java
+++ C206_CaseStudy/src/Administrator.java
@@ -1,53 +1,34 @@
 import java.util.Objects;
 
-/*
- * I declare that this code was written by me. 
- * I do not copy or allow others to copy my code. 
- * I understand that copying code is considered as plagiarism.
- * 
- * Student Name: Vishnu
- * Student ID: {type your id here}
- * Class: {type your class here}
- * Date/Time created: Monday 31-07-2023 11:23
- */
-
-/**
- * @author Vishnu
- *
- */
 public class Administrator {
-	private String fullname;
+    private String fullName;
     private String email;
     private String contactNum;
     private String customerID;
     private String accountID;
-    private String userrole;
+    private String userRole;
 
-    public Administrator(String fullname, String email, String contactNum, String customerID, String accountID, String userrole) {
-        this.fullname = fullname;
+    public Administrator(String fullName, String email, String contactNum, String customerID, String accountID, String userRole) {
+        this.fullName = fullName;
         this.email = email;
         this.contactNum = contactNum;
         this.customerID = customerID;
         this.accountID = accountID;
-        this.userrole = userrole;
+        this.userRole = userRole;
     }
-    
 
-    // Getters and setters (you can generate these using your IDE)
     public String getFullName() {
-        return fullname;
+        return fullName;
     }
 
     public String getEmail() {
         return email;
     }
-    public String getUserByEmail() {
-        return email;
-    }
+
     public String getContactNum() {
         return contactNum;
     }
-    
+
     public String getCustomerID() {
         return customerID;
     }
@@ -55,21 +36,17 @@ public class Administrator {
     public String getAccountID() {
         return accountID;
     }
+
     public String getUserRole() {
-        return userrole;
+        return userRole;
     }
-    
+
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        
-        Administrator other = (Administrator) obj;
-        return email.equals(other.email);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Administrator that = (Administrator) o;
+        return Objects.equals(email, that.email);
     }
 
     @Override
