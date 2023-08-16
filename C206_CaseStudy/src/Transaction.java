667d30c2543624e67c4f94bd9fcecf856e2f2c92
22019511 <22019511@10.175.6.135>
2023-08-16 09:46:05 +0800

    this is my transaction class

---
+++ C206_CaseStudy/src/Transaction.java
@@ -0,0 +1,42 @@
+public class Transaction {
+    private String customerID;
+    private String date;
+    private int amount;
+    private String currency;
+    private String transactionID;
+    private int balance;
+
+//constructor
+    public Transaction(String customerID, String date, int amount, String currency, int balance, String transactionID) {
+        this.customerID = customerID;
+        this.date = date;
+        this.amount = amount;
+        this.currency = currency;
+        this.balance = balance;
+        this.transactionID = transactionID;
+    }
+    //getters
+    public String getCustomerID() {
+        return customerID;
+    }
+    //getters
+    public String getDate() {
+        return date;
+    }
+    //getters
+    public int getAmount() {
+        return amount;
+    }
+    //getters
+    public String getCurrency() {
+        return currency;
+    }
+    // getters
+    public String getTransactionID() {
+        return transactionID;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
