94dcd430d81d0a0ae7a1259b1848c8ccfc0c3525
Jasraj <22036128@myrp.edu.sg>
2023-08-17 23:37:58 +0800

    Junit class

---
+++ C206_CaseStudy/test/FeedbackManager.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeedbackManager {
+    
+    private List<Feedback> feedbackList = new ArrayList<>();
+    private int feedbackIdCounter = 1;
+
+    public void addFeedback(String name, String email, String message) {
+        Feedback newFeedback = new Feedback(feedbackIdCounter++, name, email, message);
+        feedbackList.add(newFeedback);
+        System.out.println("Feedback added successfully.\n");
+    }
+
+    public void viewAllFeedbacks() {
+        if (feedbackList.isEmpty()) {
+            System.out.println("No feedbacks to display.\n");
+            return;
+        }
+        
+        System.out.println("======= All Feedbacks =======");
+        for (Feedback feedback : feedbackList) {
+            System.out.println(feedback);
+        }
+        System.out.println("=============================\n");
+    }
+
+    public void deleteFeedback(int feedbackId) {
+        Feedback feedbackToDelete = null;
+
+        for (Feedback feedback : feedbackList) {
+            if (feedback.getFeedbackId() == feedbackId) {
+                feedbackToDelete = feedback;
+                break;
+            }
+        }
+        
+        if (feedbackToDelete != null) {
+            feedbackList.remove(feedbackToDelete);
+            System.out.println("Feedback with ID: " + feedbackId + " has been deleted successfully.\n");
+        } else {
+            System.out.println("Feedback with ID: " + feedbackId + " not found. Please try again.\n");
+        }
+    }
+}
