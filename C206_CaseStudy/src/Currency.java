77f2463bf7c4100bf573e0f55c3443a4f9b65f05
sharukhot <22010784@myrp.edu.sg>
2023-08-17 15:59:45 +0800

    Currency class to initialize fields for main class

---
+++ C206_CaseStudy/src/Currency.java
@@ -0,0 +1,21 @@
+
+public class Currency {
+	
+	private String name;
+    private String code;
+
+    public Currency(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+
+}
